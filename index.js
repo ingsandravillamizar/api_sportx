2016515bf5a09a732a7f84cc0d3fca37a8540889
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-02-19 09:54:01 -0500

    tipos de identificacion

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import authRoutes from './auth/routes/authRoutes.js';
 
 
 /***  Rutas Maestros del sistema */
-import identificationTypesRoutes from './modules/master/identificationTypes/identificationTypesRoutes';
+import identificationTypesRoutes from './modules/master/identificationTypes/identificationTypesRoutes.js';
 
 
 
