d0b2bacec7a8c022bebcfa2808030ef8c958a02f
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-03-19 15:46:24 -0500

    inlcuyendo company

--- modules/master/Company/companyRoutes.js
+++ modules/master/Company/companyRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';import { apiAuth } from '../../../auth/middleware/apiauth.js';
-import { validateCreateCategory, validateGetCategory } from './categoryValidator.js';
+import { validateCreateCompany, validateGetCompany } from './companyValidator.js';
 import { createCompany, deleteCompany, getCompanies, getCompany, updateCompany } from './companyController.js';
 
 
@@ -8,8 +8,8 @@ const router = express.Router();
 
 router.get('/', apiAuth, getCompanies)
 router.get('/:id', apiAuth, validateGetCompany,  getCompany)
-router.post('/create', apiAuth, validateCreateCategory, createCompany)
-router.put('/:id', apiAuth,validateGetCategory,  updateCompany)
+router.post('/create', apiAuth, validateCreateCompany, createCompany)
+router.put('/:id', apiAuth,validateGetCompany,  updateCompany)
 router.delete('/delete/:id',  apiAuth, deleteCompany)
 
 
