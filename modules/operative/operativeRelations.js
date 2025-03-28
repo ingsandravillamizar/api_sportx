49a4393074fa09f702fde00ddcc39e24ce499bb1
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-03-28 11:48:40 -0500

    incluyendo categoria instructores

--- modules/operative/operativeRelations.js
+++ modules/operative/operativeRelations.js
@@ -3,7 +3,7 @@ import { category } from "../master/masterRelations.js";
 import categoryInstructor from "./CategoryInstructor/categoryInstructor.js";
 
 category.belongsToMany(instructor, { through: categoryInstructor, foreignKey: 'categoryId' });
-instructor.belongsToMany(category, { through: categoryInstructor, foreignKey: 'professorId' });
+instructor.belongsToMany(category, { through: categoryInstructor, foreignKey: 'instructorId' });
 
 
 export{
