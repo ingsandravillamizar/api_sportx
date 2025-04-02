477521fedf9af44c05b489a8e5605224d6e49743
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-04-02 16:45:44 -0500

    mejora a updateCategory

--- modules/master/Category/categoryController.js
+++ modules/master/Category/categoryController.js
@@ -1,6 +1,7 @@
 import { matchedData } from "express-validator";
 import { handleHttpError } from "../../../helpers/httperror.js";
 import { category}  from "../masterRelations.js";
+import { sequelize } from "../../../config/mysql.js";
 
 
 const entity = "category"
